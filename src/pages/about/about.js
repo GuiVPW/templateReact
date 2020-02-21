4300a754ad79101e0869b0b5df46f2f8d063b6a4
Xubilauu <guivpw68@gmail.com>
2020-02-21 19:15:22 -0300

    projects and about

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -82,12 +82,12 @@ class About extends Component {
                             </a>
                         </Container><br />
                         <Container id="text">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br /><br /> 
-                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
-                        sed diam nonumy eirmod tempor invid.
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+                            sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                            sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.<br /><br /> 
+                            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+                            sed diam nonumy eirmod tempor invid.
                         </Container>
                     </Container>
             </div>
